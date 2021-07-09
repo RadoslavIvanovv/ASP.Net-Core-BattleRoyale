0d7105cad335f891cb5e2aae2096b86cbdffb341
Radoslav Ivanov <redrudy@abv.bg>
2021-07-09 18:29:05 +0300

    Initial Project, ready to be modified

---
+++ BattleRoyale/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BattleRoyale.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
